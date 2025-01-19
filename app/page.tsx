f650de603ee3fff050b58a13ced636ccdd90a087
Zkael03 <azkar986@gmail.com>
2025-01-19 17:12:28 +0700

    console update

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,12 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React from "react";
 import Banner from "./components/Banner";
 import ResponsiveNav from "./components/Navbar/ResponsiveNav";
 import Footer from "./components/Footer";
 import BestSeller from "./components/BestSeller/BestSeller";
 
 export default function Home() {
-  useEffect(() => {
-    fetch('/api/connect')  // Memanggil API untuk log server-side
-      .then(response => response.json())
-      .then(data => console.log("Data dari server:", data));
-  }, []);
-
   return (
     <div>
       <ResponsiveNav />
