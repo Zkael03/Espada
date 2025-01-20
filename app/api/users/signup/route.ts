25a5f587be482b0d79f4a5e3aecd10cff455cff6
Zkael03 <azkar986@gmail.com>
2025-01-21 00:22:44 +0700

    add hashed password signup

--- app/api/users/signup/route.ts
+++ app/api/users/signup/route.ts
@@ -1,15 +1,19 @@
 import { NextResponse } from 'next/server';
 import db from "../../../../lib/db";
-import { OkPacket } from 'mysql2'; // OkPacket untuk hasil query INSERT
+import { OkPacket } from 'mysql2';
+import bcrypt from 'bcryptjs'; // Import bcryptjs untuk hashing password
 
 export async function POST(request: Request) {
   try {
     const body = await request.json();
     const { username, email, phone, password } = body;
 
+    // Hash password menggunakan bcrypt
+    const hashedPassword = await bcrypt.hash(password, 10); // 10 adalah salt rounds
+
     // Query untuk insert data
     const query = 'INSERT INTO users (username, email, nomor_telepon, password, created_at) VALUES (?, ?, ?, ?, NOW())';
-    const values = [username, email, phone, password];
+    const values = [username, email, phone, hashedPassword]; // Ganti password dengan hashedPassword
 
     return new Promise((resolve, reject) => {
       db.query(query, values, (error, results: OkPacket) => {
